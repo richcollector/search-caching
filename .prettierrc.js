655e80d8af2456846bbdca20c4985b9d4b34154a
richcollector <ktw9115@naver.com>
2023-09-06 10:55:05 +0900

    initial: react 생성 및  eslint / prettier 설정 related to: #1

---
+++ .prettierrc.js
@@ -0,0 +1,9 @@
+module.exports = {
+  printWidth: 100, // printWidth default 80 => 100 으로 변경
+  singleQuote: true, // "" => ''
+  arrowParens: "avoid", // arrow function parameter가 하나일 경우 괄호 생략
+  semi: true, // 코드의 끝에 세미콜론 사용
+  useTabs: true, // 들여쓰기 사용
+  tabWidth: 2, // 들여쓰기 공백 수
+  trailingComma: "all", // 모든 문법 끝에 ,(콤마) 사용
+};
